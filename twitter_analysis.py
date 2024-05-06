b3cdb50f988c3bd27a61840709734f67c8a0766c
Jeriylflorence <jeriyl94@gmail.com>
2024-05-06 18:54:54 +0530

    Update twitter_analysis.py

--- twitter_analysis.py
+++ twitter_analysis.py
@@ -6,13 +6,12 @@ from nltk.corpus import stopwords
 import re
 from streamlit_lottie import st_lottie
 import requests
-import nltk
-nltk.download('stopwords')
 
-st.set_page_config(page_title="Twitter Sentiment Analysis",page_icon=":blue_heart:")
+st.set_page_config(page_title="Twitter Sentiment Analysis",page_icon=":blue_heart:",layout="wide")
 def setting_bg():
     st.markdown(f""" <style>.stApp {{
-                        background: linear-gradient(to bottom, #87CEEB, #4682B4);}}
+                        background:url("https://c4.wallpaperflare.com/wallpaper/154/47/615/twitter-social-networks-wallpaper-preview.jpg");
+                        background-size: cover}}
                      </style>""", unsafe_allow_html=True)
 setting_bg()
 def load_lottieurl(url):
@@ -21,7 +20,7 @@ def load_lottieurl(url):
         return None
     return r.json()
 
-lottie_coding=load_lottieurl("https://lottie.host/304662f4-751f-4ec6-bf94-5633f32de03e/aOWpvskzxm.json")
+#lottie_coding=load_lottieurl("https://lottie.host/304662f4-751f-4ec6-bf94-5633f32de03e/aOWpvskzxm.json")
 
 # Load the pre-trained model
 loaded_model = pickle.load(open(r'trained_model_logisticRegression.pkl', "rb"))
@@ -46,11 +45,7 @@ title_column, lottie_column = st.columns([1, 0.3])
 with title_column:
     st.markdown("<p style='font-size:38px; color:White; text-align:right; font-weight:bold;'>Twitter Sentiment Analysis</p>", unsafe_allow_html=True)
 
-# Lottie animation in the right column
-with lottie_column:
-    st_lottie(lottie_coding, height=80, key="coding")
-
-tweet = st.text_area("Enter a tweet:", height=200)
+tweet = st.text_area("Enter a tweet:", height=170)
 
 if st.button("Predict the Status"):
     # Preprocess the input tweet
@@ -64,11 +59,13 @@ if st.button("Predict the Status"):
 
     # Display the sentiment
     if prediction[0] == 0:
-        st.error("Negative Tweet")
-    else:
-        st.success("Positive Tweet")
+        st.markdown('<p style="color:red; font-size: 24px; font-family: Arial, sans-serif; font-style: italic; font-weight: bold; border: 1px solid black; padding: 10px; text-align: center; background-color: pink;">Negative Tweet</p>', unsafe_allow_html=True)
 
-st.markdown("<p style='font-size:28px; color:White; font-weight:bold;'>Sample Tweets</p>", unsafe_allow_html=True)
+
+    else:
+        st.markdown('<p style="color:green; font-size: 24px; font-family: Arial, sans-serif; font-style: italic; font-weight: bold; border: 1px solid black; padding: 10px; text-align: center; background-color: lightgreen;">Positive Tweet</p>', unsafe_allow_html=True)
+
+st.markdown("<p style='font-size:28px; color:White; font-weight:bold;'>Sample Tweets:</p>", unsafe_allow_html=True)
 
 # Display the sample tweets
 sample_tweets = [
@@ -77,6 +74,6 @@ sample_tweets = [
     "Today has been a rough day. Nothing seems to be going right. 😔 #BadDay"
 
 ]
-
 for idx, sample_tweet in enumerate(sample_tweets, start=1):
-    st.write(f"{idx}. {sample_tweet}")
+    st.write(f"<span style='color:white;font-size:18px'>{idx}. {sample_tweet}</span>", unsafe_allow_html=True)
+
